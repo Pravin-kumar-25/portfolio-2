7f6b20871a0e9b9e9fd19ff010a912f2aabc276a
Pravin-kumar-25 <pravinkumar3725@gmail.com>
2022-08-03 18:48:26 +0530

    Add scroll trigger animation on projects

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -1,18 +1,65 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React,{useEffect, useRef} from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 const ProjectImage = ({ item,index }) => {
+    gsap.registerPlugin(ScrollTrigger)
+    const descRef = useRef()
+    const imageRef = useRef()
+    useEffect(() => {
+      gsap.from(descRef.current,{
+          opacity:0,
+          x:index%2 !==0 ? '-100px':'100px',
+          scrollTrigger:{
+              trigger:descRef.current,
+              scrub:1,
+              start:'20px 90%',
+              end:'bottom 80%',
+              toggleActions:'restart none reverse none',
+            //   markers:true
+          }
+      })
+      gsap.from(imageRef.current,{
+        opacity:0,
+        x:index%2 !==0 ? '150px':'-150px',
+        scrollTrigger:{
+            trigger:imageRef.current,
+            start:'top 90%',
+            end:'90% 80%',
+            scrub:1,
+            toggleActions:'restart none reverse none',
+            // markers:true,
+            // id:'images'
+        }
 
+    })
+    })
+
+    const onMouseEnter = () => {
+        gsap.to(imageRef.current, {
+            scale:0.95,
+            duration:'1s'
+        })
+    }
+
+    const onMouseLeave = () => {
+        gsap.to(imageRef.current, {
+            scale:1,
+            duration:'1s'
+        })
+    }
+    
     return (
         <li
             className={`projectDiv ${index%2!==0 ? 'invertedProjectDiv': ''}`}
         >
-            <div className='projectDescription'>
+            <div className='projectDescription' ref={descRef} >
                 <h3>{item.title}</h3>
                 <p>{item.description}</p>
                 <a href={item.link}>Take a look...</a>
             </div>
-            <div className='projectImage'>
+            <div className='projectImage' ref={imageRef} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
                 <img src={item.img} alt='images' />
             </div>
         </li>
