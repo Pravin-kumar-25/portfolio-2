afb36144f7671ca8254d695db7d5c207e212a17a
Pravin-kumar-25 <pravinkumar3725@gmail.com>
2022-08-04 12:34:04 +0530

    Add scroll animation to description

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -1,14 +1,45 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import { useEffect,useRef } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 const ProjectImage = ({ item,index }) => {
+    const descRef = useRef(null)
+
+    useEffect(()=> {
+        const descEl = descRef.current
+        gsap.fromTo(descEl, {
+            opacity:0,
+            x:'100px' ,
+        }, {
+            opacity:1,
+            x:0,
+            scrollTrigger:{
+                trigger:descEl,
+                toggleActions:'restart none none reset',
+                start:'top 80%'
+            }
+        })
+        // gsap.fromTo(imageEl, {
+        //     opacity:0,
+        //     x:'-100px' 
+        // }, {
+        //     opacity:1,
+        //     x:0,
+        //     scrollTrigger:{
+        //         trigger:imageEl
+        //     }
+        // })
+    },[])
     
     return (
 
         <li
             className={`projectDiv ${index%2!==0 ? 'invertedProjectDiv': ''}`}
         >
-            <div className='projectDescription' >
+            <div className='projectDescription' ref={descRef}>
                 <h3>{item.title}</h3>
                 <p>{item.description}</p>
                 <a href={item.link}>Take a look...</a>
